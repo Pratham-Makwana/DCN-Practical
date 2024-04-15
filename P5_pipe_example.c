05a651eb6038d2a532248e23d24c6e4e0da413d5
Pratham Makwana <makwanapratham13@gmail.com>
2024-04-15 14:38:00 +0530

    progte a pipe and send “Hello” message

---
+++ P5_pipe_example.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<string.h>
+
+void main()
+{
+
+	int n;
+	char buff[1024];
+	int flag[2];
+	char *data="hello how are you";
+	//pipe(flag);
+
+	write(flag[1],data,strlen(data));
+
+	n=read(flag[0],buff,1024);
+	int i;
+
+	for(i=0;i<strlen(buff);i++)
+	{
+		printf("%c",buff[i]);
+
+	}
+	printf("\n");
+
+}
